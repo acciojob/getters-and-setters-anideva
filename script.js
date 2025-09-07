4cf63dcc2d2b32e3686d86c084c7ac71faaac406
Aniketh Roy Chowdhury <95467658+anideva@users.noreply.github.com>
2025-09-07 19:27:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,59 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+	}
+	//getter for the name 
+	get name(){
+		return this.name;
+	}
 
-class Student extends Person {}
+	//getter for the age 
+	get age(){
+		return this.age;
+	}
 
-class Teacher extends Person {}
+	//setter for the age
+	set age(newAge){
+		this.age = newAge;
+	}
+}
+
+class Student extends Person {
+	constructor (name,age) {
+		super(name,age); //parent constructor seh call kar raha hai 
+	}
+
+	study(){
+		console.log(`${this.name} is studying `);
+	}
+}
+
+class Teacher extends Person {
+	constructor(name,age) {
+		super(name,age); //yeh bhi main constructor seh call kar raha hai 
+	}
+
+	teach() {
+		console.log(`{this.name} is teaching `);
+	}
+}
+
+const person = new Person("John", 25);
+console.log(person.name); 
+
+person.age = 30;  // Using the setter to change the age
+console.log(person.age);  
+
+// Creating a Student instance
+const student = new Student("Alice", 22);
+student.study();  
+
+// Creating a Teacher instance
+const teacher = new Teacher("Bob", 40);
+teacher.teach();  
+
 
 // Do not change the code below this line
 window.Person = Person;
