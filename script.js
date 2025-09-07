d0c2f4d0145804f301ea80f79fed03639862a3c6
Aniketh Roy Chowdhury <95467658+anideva@users.noreply.github.com>
2025-09-07 19:31:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -40,19 +40,6 @@ class Teacher extends Person {
 	}
 }
 
-const person = new Person("John", 25);
-console.log(person.name); 
-
-person.age = 30;  // Using the setter to change the age
-console.log(person.age);  
-
-// Creating a Student instance
-const student = new Student("Alice", 22);
-student.study();  
-
-// Creating a Teacher instance
-const teacher = new Teacher("Bob", 40);
-teacher.teach();  
 
 
 // Do not change the code below this line
